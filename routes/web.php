d297be16e16f78629c96f9b4ef20deae4b865e60
Fernando Artur Luithardt <fluithardt@gmail.com>
2023-01-11 21:54:39 -0300

    prototype index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\IndexController;
+Route::get('/', [IndexController::class, 'index']);
