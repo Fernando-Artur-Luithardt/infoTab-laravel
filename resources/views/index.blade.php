bd602995a007fa73a97ea39a3f6d9d71690985d8
Fernando Artur Luithardt <fluithardt@gmail.com>
2023-01-16 22:04:26 -0300

    css from index/home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,7 @@
   <link rel="stylesheet" href="{{asset('site/bootstrap.css')}}" />
   <link rel="stylesheet" href="{{asset('site/index.css')}}" />
   <script src="{{asset('site/jquery.js')}}"></script>
+  <script src="https://use.fontawesome.com/8d78ec2fc5.js"></script>
 </head>
 <body>
   <!-- Responsive navbar-->
@@ -31,23 +32,44 @@
     </div>
   </nav>
   <!-- Page Content-->
-  <div class="container px-4 px-lg-5">
+  <div class="container px-5 px-lg-6">
     <!-- Heading Row-->
-    <div class="row gx-4 gx-lg-5 align-items-center my-5">
-      <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0"
-          src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
+    <div class="row gx-4 gx-lg-5 align-items-center my-5" style="min-height: 350px;">
+        <div class="col-lg-7">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
       <div class="col-lg-5">
-        <h1 class="font-weight-light">InfoTab</h1>
+        <h1 class="font-weight-light">Soluções em informática e sugurança eletrônica</h1>
         <p>Soluções em informática e sugurança eletrônica, trabalhamos com Computadores gamer, worksation, Instalação e
           manutenção de câmeras de segurança, venda e manutenção de no-break</p>
         <a class="btn btn-green" href="https://wa.me/554735227750"><i class="fa fa-whatsapp"></i> WhattsApp</a>
+        <a class="btn btn-instagram" href="https://www.instagram.com/infotab_info/"><i class="fa fa-instagram"></i> Instagram</a>
       </div>
     </div>
     <!-- Call to Action-->
     <div class="my-5 py-4 text-center">
-      <hr>
-      <div class="font-weight-light" style="padding-top: 12   px;">
-        <H2>NOSSOS SERVIÇOS</H2>
+      <div class="font-weight-light" style="padding-top: 20px;">
+        <H1>Nossos Serviços</H1>
       </div>
     </div>
     <!-- Content Row-->
@@ -64,7 +86,7 @@
       <div class="col-md-4 mb-5">
         <div class="card h-100">
           <div class="card-body">
-            <h2 class="card-title">COMPUTADORES GAMER</h2>
+            <h2 class="card-title">Computadores gamer</h2>
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at
               dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi
               labore.</p>
@@ -86,7 +108,7 @@
   </div>
   <!-- Footer-->
   <footer class="py-5 bg-dark">
-    <div class="container px-4 px-lg-5">
+    <div class="container px-5 px-lg-6">
       <p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p>
     </div>
   </footer>
